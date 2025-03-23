463f65de639284e9f5aad238a59a14bf577135db
EmpSwarup <empswarupcoc@gmail.com>
2025-03-23 16:01:00 +0545

    feat: add Footer and Navbar components

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,11 @@
+function Navbar() {
+  return (
+    <nav className=" text-black p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-xl font-bold">RENT</h1>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
