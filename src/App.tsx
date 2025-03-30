5d4951e6c1453406571b10d8d7148963c8115829
EmpSwarup <empswarupcoc@gmail.com>
2025-03-30 13:58:58 +0545

    feat: refactor App component to use EarthquakeMap and simplify main rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import "./App.css";
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
-import Map from "./components/Map";
+import EarthquakeMap from "./components/EarthquakeMap";
 
 function App() {
   return (
-    <div className="container mx-auto px-2 md:px-4 py-2 md:py-4 flex flex-col min-h-screen">
-      <Navbar />
-      <main className="flex-grow">
-        <Map />
-      </main>
-      <Footer />
+    <div className="min-h-screen bg-background">
+      <EarthquakeMap />
     </div>
   );
 }
