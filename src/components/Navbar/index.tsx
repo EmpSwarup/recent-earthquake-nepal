e3a83fffb48f6d1610f617dc67906ddb7792b463
EmpSwarup <empswarupcoc@gmail.com>
2025-03-30 12:07:44 +0545

    feat: enhance Footer and Navbar components with improved layout and additional information

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,8 +1,13 @@
 function Navbar() {
   return (
-    <nav className=" text-black p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-xl font-bold">RENT</h1>
+    <nav className="text-black p-4">
+      <div className="mb-3 md:mb-6">
+        <h1 className="text-xl md:text-3xl font-bold tracking-tight">
+          Recent Earthquake in Nepal Tracker
+        </h1>
+        <p className="text-xs md:text-sm text-muted-foreground">
+          Visualize recent earthquake activity in Nepal and surrounding regions
+        </p>
       </div>
     </nav>
   );
